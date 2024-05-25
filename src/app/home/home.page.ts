ff7e4c9121bcf6c6936ab2e334f03a2ddd6ef963
Caio Ferreira <Mouraaum@gmail.com>
2024-05-25 10:51:55 -0300

    firts commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CepService } from '../cep/cep.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  dados: any = "";
+  cep: string = "";
 
+  constructor(
+    private cepService: CepService,
+    private alertController:AlertController
+  ){}
+
+
+  buscar(cep: string) {
+    this.cepService.buscarCep(cep.replace(/[^0-9]/g, '')).subscribe((data: any) => {
+          this.dados = data;
+            const card: any = document.getElementsByClassName("card")[0];
+            card.style.display = "block"; 
+        },
+        (error: any) => {
+          console.error('Erro ao buscar endereço:', error);
+        });
+  }
 }
